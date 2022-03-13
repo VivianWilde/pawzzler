d3c5c4b5807b080bd9ea68a05888600e0705294b
Preshtha Garg <84159177+preshthagarg@users.noreply.github.com>
2022-03-12 23:39:27 -0800

    set up most of the tictactoe logic

---
+++ tictactoe/basic_game_test.py
@@ -0,0 +1,14 @@
+from tictactoe import TicTacToe
+
+my_game = TicTacToe()
+
+print(my_game)
+my_game.player_move()
+print(my_game)
+my_game.ai_move()
+print(my_game)
+my_game.player_move()
+my_game.ai_move()
+print(my_game.over())
+print(my_game.player_victory())
+print(my_game)
