2d922bce61631d254331a08e4b94317ea4124967
Rohan Goyal <rohan.goyal@berkeley.edu>
2022-03-14 15:39:11 -0700

    Locations, bit of a mess

--- game.py
+++ game.py
@@ -1,15 +1,42 @@
 import cocos
 import numpy as np
 
-#Making a tic-tac-toe game
+
+class MiniGame:
+    def __init__(self, args):
+        state = f(args)
+
+    def play():
+        while not self.over():
+            self.player_move()
+            if not self.over():
+                self.ai_move()
+        return self.player_wins()
+
+    def over():
+        pass
+
+    def player_move():
+        pass
+
+    def ai_move():
+        pass
+
+    def player_wins():
+        pass
+
+
+
+# Making a tic-tac-toe game
 #
+
 
 class TicTacToe(cocos.layer.Layer):
 
-    #Representation -
-    #board is a list of lists
-    #pieces are crosses(1) or circles(2) or empty(3)
-    #empty spaces are None
+    # Representation -
+    # board is a list of lists
+    # pieces are crosses(1) or circles(2) or empty(3)
+    # empty spaces are None
     #
 
     def __init__():
@@ -17,20 +44,12 @@ class TicTacToe(cocos.layer.Layer):
         self.board = np.zeroes(3)
         self.who = 1
 
-
-    def play():
-        while(true):
-            if not game_over():
-                x_direction = int(input(f"Player {self.who}'s turn. x coordinate of your next move:"))
-                y_direction = int(input(f"y coordinate of your next move:"))
-                self.place(x, y)
-                self.next_player()
-            else:
-                return
-
     def game_over():
+        while true:
         return self.board_full() or self.win_condition()
-
+                x_direction = int(
+                    input(f"Player {self.who}'s turn. x coordinate of your next move:")
+                )
     def board_full(self):
         for i in range(3):
             for j in range(3):
@@ -38,7 +57,7 @@ class TicTacToe(cocos.layer.Layer):
                     return False
         return True
 
-    def win_condition
+    def win_condition(self):
         #Three parts - a row, a column or a diagnol
         for row in self.rows:
             if sum(row) == 6:
@@ -47,11 +66,10 @@ class TicTacToe(cocos.layer.Layer):
             if sum(row) == 3:
                 self.winner = 1
                 return False
-        for col in range(len(3))
+        for col in range(len(3)):
+        # Three parts - a row, a column or a diagnol
 
 
     def place(self, x, y):
         self.board[y][x] = self.who
 
-    def next_player():
-        self.who = abs(self.who -1)
