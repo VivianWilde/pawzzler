7d68466fc5c3755aa24d9e349ca34c933b7781c9
Preshtha Garg <84159177+preshthagarg@users.noreply.github.com>
2022-03-28 22:31:47 -0700

    worked on Character and gamestate

---
+++ gamestate.py
@@ -0,0 +1,15 @@
+
+
+
+class Gamestate:
+
+    def __init__(self, protag,):
+        self.protag = protag
+        self.map = #make new map magic
+        default_start = None #make default start location magic
+        self.map.add(default_start, x, y)
+
+
+    def new_game(self):
+        name, appearance, pronouns = #magic(frontend)
+        protag = PC(name, appearance, pronouns)
