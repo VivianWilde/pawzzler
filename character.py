e65bc2c99786e3ea8251199338369de5a0cd512d
Preshtha Garg <84159177+preshthagarg@users.noreply.github.com>
2022-04-16 15:41:20 -0700

    gamestate configuration

--- character.py
+++ character.py
@@ -1,28 +1,27 @@
 
 class Character:
-    def __init__(self, appearance, map, x, y):
+    def __init__(self, appearance):
         self.appearance = appearance
-        self.map = map
-        self.map.add(self, map, x, y)
-        self.loc = (x, y)
+        self.map = None
+        self.loc = None
         pass
 
     def move(self, x_dir, y_dir):
         self.location.move()
 
 class NPC(Character, Interactable):
-    def __init__(self, appearance, map, dialogue=None):
-        super.__init__(appearance, map)
+    def __init__(self, appearance, dialogue=None):
+        super.__init__(appearance)
         self.dialogue = dialogue
         pass
 
 
 class Creature(NPC):
-    def __init__(self, appearance, map, dialogue):
-        super().__init__(appearance, map, dialogue)
+    def __init__(self, appearance, dialogue):
+        super().__init__(appearance, dialogue)
 
 class PC(Character):
-    def __init__(self, name, appearance, pronouns, start_map, x, y):
-        super(PC, self).__init__(self, start_map, appearance, x, y)
+    def __init__(self, name, appearance, pronouns, start_map):
+        super(PC, self).__init__(self, , appearance)
         self.name = name
         self.pronouns = pronouns
--- gamestate.py
+++ gamestate.py
@@ -4,11 +4,12 @@ from character import Character, PC, NPC, Creature
 
 class Gamestate:
 
-    def __init__(self, protag, world=None, start_map=None):
+    def __init__(self, protag, world=None, start_map=None, gui):
         self.protag = protag
         self.world = world #make new map magic
         self.world.add(start_map, 5, 5)
         self.map = start_map
+        self.gui = gui
 
     @staticmethod
     def new_game():
@@ -17,14 +18,21 @@ class Gamestate:
         protag = PC(name, appearance, pronouns)
         protag.loc = locations.center(world)
         start_map = Location(32, 20, "floor")
-        gamestate = Gamestate(protag, world, start_map)
+        gui = GUI()
+        gamestate = Gamestate(protag, world, start_map, gui)
         gamestate.initialize_game()
         return gamestate
 
 
-    def initialize_game_from_file(self, level=0):
+    def initialize_game(self, file, level=0):
         cat_dialogue = 
         cat = Creature("Cat image", dialogue=)
         self.world.add(cat)
+
+    def initialize_game_testing(self, level=0):
+
+
+    def get_command(self):
+        self.gui.get_command
 
 
