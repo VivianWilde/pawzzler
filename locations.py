0bf2257825337cce64661d63d09fea18928510d5
Rohan Goyal <rohan.goyal@berkeley.edu>
2022-05-25 13:56:29 -0700

    Tinker with locations, simplified characters, broke interactions

--- locations.py
+++ locations.py
@@ -1,24 +1,36 @@
-    #!/usr/bin/env ipython3
+#!/usr/bin/env ipython3
 class Location:
-    def __init__(self) -> None:
-        self.loc=(0,0)
-        self.mapdict={}
-        self.WIDTH=1440
-        self.WIDTH=1024
+    def __init__(self, width=100, height=100, texture="void", start=(0, 0)):
+        self.loc = start
+        self.mapdict = {}
+        self.WIDTH = width
+        self.HEIGHT = height
+        self.texture = texture
 
-    def unblocked(self,pos):
+    def add_character(self, x, y):
+        self.loc = (x, y)
+
+    def remove_character(self):
+        self.loc = None
+
+    def add(self, item, x, y):
+        self.create_item(item, (x, y))
+
+    def create_item(self, item, pos):
+        self.mapdict[pos] = item
+
+    def unblocked(self, pos):
         return pos in self.mapdict
 
     def in_range(self, pos):
-        return 0<=pos[0]<=self.WIDTH and 0<=pos[1]<=self.HEIGHT
-
+        return 0 <= pos[0] <= self.WIDTH and 0 <= pos[1] <= self.HEIGHT
 
     def movable(self, direction):
-        pos=self.to_absolute(direction)
+        pos = self.to_absolute(direction)
         return self.unblocked(pos) and self.in_range(pos)
 
     def update(self, direction):
-        self.loc=self.to_absolute(direction)
+        self.loc = self.to_absolute(direction)
 
     def try_interact(self):
         for cell in self.adjacents():
@@ -26,19 +38,19 @@ class Location:
                 self.mapdict[cell].interact()
 
     def adjacents(self):
-        directions=[(0,1),(1,0),(0,-1),(-1,0)]
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         return map(self.to_absolute, directions)
 
     @staticmethod
-    def vec_add(a,b):
-        map(lambda i: a[i]+b[i], range(len(a)))
+    def vec_add(a, b):
+        map(lambda i: a[i] + b[i], range(len(a)))
 
     @staticmethod
-    def vec_sub(a,b):
-        map(lambda i: a[i]-b[i], range(len(a)))
+    def vec_sub(a, b):
+        map(lambda i: a[i] - b[i], range(len(a)))
 
     def to_absolute(self, pos):
-        return self.vec_add(pos,self.loc)
+        return self.vec_add(pos, self.loc)
 
     def to_relative(self, pos):
         return self.vec_sub(pos, self.loc)
