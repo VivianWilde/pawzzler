01c729ae4bdf246de1ecb5ff7407fd892a7fa3da
Preshtha Garg <84159177+preshthagarg@users.noreply.github.com>
2022-05-26 13:18:38 +0530

    edited main

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import gamestate
 
 def main():
     gui = GUI()
-    #gui needs to 1) show a screen with options for new game/from saved game
+    gui.show_splash_screen()
+    #gui needs to 1) show a screen with options for new game/from saved game/settings
     # 2) on new game, call Gamestate.new_game(self) -> return gamestate
 
     # 3) wait for player input.
