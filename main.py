f6a869a51ba7754cf4debca753cd5c9ff6b61a9b
Preshtha Garg <84159177+preshthagarg@users.noreply.github.com>
2022-05-26 13:27:05 +0530

    main

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import gamestate
 
 def main():
     gui = GUI()
-    gui.show_splash_screen()
+    gui.intialize()
     #gui needs to 1) show a screen with options for new game/from saved game/settings
     # 2) on new game, call Gamestate.new_game(self) -> return gamestate
 
     # 3) wait for player input.
+if __name__ == "__main__":
+    main()
