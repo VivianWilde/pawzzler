7b211dc2c8332f37822da3c4875e9d32ce0d0b13
Rohan Goyal <rohan.goyal@berkeley.edu>
2022-05-27 01:55:06 -0700

    Interactions now uses digraph, makes a bit more sense

--- interactions.py
+++ interactions.py
@@ -66,32 +66,56 @@ class InteractableObject:
     # DONE: How to populate the tree?
     # TODO: Refactor to use the digraph library
     # TODO: Way to check if a path is valid - are we allowed to continue from the current point?
-    def __init__(self, prompt, gamestate, appearance): # appearance is a filepath
-        self.dialogue_tree = DiGraph()
-        self.current = (
-            self.dialogue_tree
-        )  # Pointer to where the user is in the dialogue tree.
+    def __init__(self, scenes, gamestate, appearance): # appearance is a filepath
+        self.scenes=scenes # List of digraphs
+        self.scene_index=0
+        self.current_id = self.get_root_node() # Pointer to where the user is in the dialogue tree.
         self.appearance = appearance
         self.gamestate=gamestate
+
+        @property
+        def dialogue_tree(self):
+            return self.scenes[self.scene_index]
+
+        @property
+        def current(self):
+            return self.dialogue_tree[self.current_id]
 
         # DONE: Do we want to immediately exit upon hitting a leaf, or do we want an option for NPC to give a final message/warning or something. Ominous prophecies are important.
         # DONE: Comprehensive story for feedback - NPC response/acknowledgement of character choices. So the idea is Npc-prompt -> character chooses a response -> display choice and npc response -> repeat.
     def interact(self):
-        self.gamestate.gui.display_line(self.current.response)
+        self.gamestate.gui.display_line(self.current["response"])
         self.current["callback"]()
-        if len(self.branches()) == 0:
-            self.exit_interaction()
+        if self.at_leaf():
+            self.next_interaction()
             return
-        chosen_idx = self.gamestate.gui.dialogue(self.current.branches)
-        chosen_option = self.branches()[chosen_idx]
-        self.current = chosen_option
+        chosen_idx = self.gamestate.gui.dialogue(self.branches)
+        chosen_option = self.branches[chosen_idx]
+        self.current_id = chosen_option # TODO: get label if necessary
         self.interact()
 
-    def exit_interaction(self):
-        self.current = self.dialogue_tree
-
+    @property
     def branches(self):
-        return self.dialogue_tree.adjacency()[self.current]
+        return list(self.dialogue_tree.successors(self.current_id))
+
+
+    def get_root_node(self):
+        return self.dialogue_tree.nodes["BEGIN"]
+    # TODO: In parser, BEGIN is a special heading (breaks rules for labelling)
+
+    def at_leaf(self):
+        valid_options=filter(lambda x: x["valid"](), self.branches)
+        return len(list(valid_options))==0
+
+
+    def next_interaction(self):
+        if self.next_exists():
+            self.scene_index+=1 # TODO: Bounds checks
+        self.current_id=self.get_root_node()
+
+    def next_exists(self):
+        return self.scene_index+1<len(self.scenes)
+
 
 class Door(InteractableObject):
     def __init__(self, new_map, image=None):
